a8ea1b23521430759b5075fdb91279f883b08966
RyanBraga <125956400+ryanbragaa@users.noreply.github.com>
2023-11-08 18:41:16 -0300

    adding file base commands

---
+++ base/commandBase.js
@@ -0,0 +1,21 @@
+const Discord = requere("discord.js");
+
+module.exports = {
+    name: "",
+    description: "",
+    type: Discord.ApplicationCommandType.ChatInput,
+    options: [
+        {
+            name: "",
+            description: "",
+            type: Discord.ApplicationCommandType,
+            required: true,
+        }
+    ],
+
+    run: async (client, interaction) => {
+
+
+        
+    }
+}
