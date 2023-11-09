fe102cb3b67475ef6c62a9a9cddd707049545d75
RyanBraga <125956400+ryanbragaa@users.noreply.github.com>
2023-11-09 12:44:51 -0300

    command changes

--- Commands/entreterimento/playlist.js
+++ Commands/entreterimento/playlist.js
@@ -3,11 +3,11 @@ const config = require('../../config.json');
 
 module.exports = {
     name: "playlist",
-    description: "Playlist para estudos",
+    description: "Playlist Da Nistros",
     type: Discord.ApplicationCommandType.ChatInput,
 
     run: async (client, interaction) => {
         
-    await interaction.reply({content:"https://open.spotify.com/playlist/0vvXsWCC9xrXsKd4FyS8kM?si=bdfa0dfa494940bf"})
+    await interaction.reply({content:"https://open.spotify.com/playlist/32YGutOLaicCbIfZpCkxXp?si=dde232c0230c4024"})
     }
 }
