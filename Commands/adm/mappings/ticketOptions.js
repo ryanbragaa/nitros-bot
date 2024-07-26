20f078c4cdf28557263b31fd73ec12c7eb41d407
Ryan Braga <ryan.cb2012@gmail.com>
2024-07-26 14:44:37 -0300

    add comand ticket Options

---
+++ Commands/adm/mappings/ticketOptions.js
@@ -0,0 +1,17 @@
+// mappings/ticketOptions.js
+
+module.exports = {
+    "opc1": "Instagram",
+    "opc2": "TikTok",
+    "opc3": "YouTube",
+    "opc4": "Ativação Nitro",
+    "opc5": "Conta-Discord",
+    "opc6": "Nitro-Mensal",
+    "opc7": "Nitro-Trimensal",
+    "opc8": "Nitro-Gift-Mensal",
+    "opc9": "Serviços de Streaming",
+    "opc10": "Sala personalizada Free Fire",
+    "opc11": "Ativação Windows",
+    "opc12": "Cidade MTA",
+    "opc13": "Bots Discord"
+};
